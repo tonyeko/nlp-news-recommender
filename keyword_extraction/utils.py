3a3253f0c63b9919c14c0052c8229dbc8b019e96
rifaldyaristya23 <13518082@std.stei.itb.ac.id>
2021-11-15 01:25:09 +0700

    add number of keywords to be generated

--- keyword_extraction/utils.py
+++ keyword_extraction/utils.py
@@ -37,24 +37,51 @@ def fullyTokenizeSentences(tokenizer, sentences, tokenized_sentences):
     input_ids.append(encoded_dict['input_ids'][0])
     attention_masks.append(encoded_dict['attention_mask'][0])
   
-  return input_ids, attention_masks
+  return input_ids, attention_masks, max_len
 
-def get_list_of_keywords(tokenized_sentences, model, input_ids, attention_masks):
+def flat_tokenized_sentences(tokenized_sentences, maxlen):
+  flatten_tokenized_sentences = []
+  for tokenized_sentence in tokenized_sentences:
+    flatten_tokenized_sentences.append("[CLS]")
+    for token in tokenized_sentence:
+      flatten_tokenized_sentences.append(token)
+    flatten_tokenized_sentences.append("[SEP]")
+    num_of_padding = maxlen-len(tokenized_sentence)
+    for i in range(num_of_padding):
+      flatten_tokenized_sentences.append("padding")
+  
+  return flatten_tokenized_sentences
+
+def get_list_of_keywords(tokenized_sentences, model, input_ids, attention_masks, maxlen, nKeywords):
   input_ids = torch.stack(input_ids)
   attention_masks = torch.stack(attention_masks)
-  prediction = model(input_ids, attention_mask=attention_masks)[0].detach().numpy()
-  print("=============================")
-  print(prediction.shape)
-  print("=============================")
-  prediction = np.argmax(prediction, axis=2)
+  prediction = model(input_ids, attention_mask=attention_masks)
+  prediction = prediction[0].detach().numpy()
   keywords=[]
-  for i in range(len(tokenized_sentences)):
-    for j in range(len(tokenized_sentences[i])):
-      if(prediction[i][j+1]==1): #+1 karena ada token CLS di awal sentence
-        keywords.append(tokenized_sentences[i][j])
+
+  flatten_tokenized_sentences = flat_tokenized_sentences(tokenized_sentences, maxlen)
+
+  if(nKeywords==0):
+    prediction = np.argmax(prediction, axis=2)
+    for i in range(len(tokenized_sentences)):
+      for j in range(len(tokenized_sentences[i])):
+        if(prediction[i][j+1]==1): #+1 karena ada token CLS di awal sentence
+          keywords.append(tokenized_sentences[i][j])
+  else:
+    flatten_prediction = prediction.reshape(prediction.shape[0]*prediction.shape[1], prediction.shape[2])[:,1]
+    indices = (-flatten_prediction).argsort()
+    num_of_entries=0
+    for idx in indices:
+      keyword = flatten_tokenized_sentences[idx]
+      if(keyword not in keywords):
+        keywords.append(keyword)
+        num_of_entries += 1
+      if(num_of_entries==nKeywords):
+        break
+
   return keywords
 
-def get_text_keywords(text):
+def get_text_keywords(text, num_of_keywords=0):
 
   config_class, model_class, tokenizer_class = (BertConfig, AutoModelForTokenClassification, BertTokenizer)
   model = model_class.from_pretrained("saved_model/")
@@ -62,6 +89,10 @@ def get_text_keywords(text):
 
   sentences = tokenize.sent_tokenize(text)
   tokenized_sentences = tokenizeSentences(tokenizer, sentences)
-  input_ids, attention_masks = fullyTokenizeSentences(tokenizer, sentences, tokenized_sentences)
-  list_of_keywords = list(set(get_list_of_keywords(tokenized_sentences, model, input_ids, attention_masks)))
+  #Check if tokenized_sentences > 500 words cut that tokens (embedding layer max tokens is 512)
+  for j in range(len(tokenized_sentences)):
+    if(len(tokenized_sentences[j])>500):
+      tokenized_sentences[j] = tokenized_sentences[j][:500]
+  input_ids, attention_masks, maxlen = fullyTokenizeSentences(tokenizer, sentences, tokenized_sentences)
+  list_of_keywords = list(set(get_list_of_keywords(tokenized_sentences, model, input_ids, attention_masks, maxlen, num_of_keywords)))
   return list_of_keywords
