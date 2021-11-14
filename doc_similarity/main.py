c9afb86a7a3e981a77aeb6b17b28a48260e2d1b8
tony <tonyeko@users.noreply.github.com>
2021-11-14 23:59:54 +0700

    add model transformer

--- doc_similarity/main.py
+++ doc_similarity/main.py
@@ -1,6 +1,7 @@
 import os
 from doc_similarity.model_transformer_embed import BertModel
 from doc_similarity.model_tfidf import tfidf_recommendation
+from doc_similarity.model_transformer import transformer_recommendation
 
 project_path = os.getcwd()
 
@@ -8,4 +9,5 @@ def recommend(news_keywords_arr):
   # bert_model = BertModel()
   # bert_model.load(f'{project_path}/saved_models/bert_embed_mat.npy')
   # bert_model.recommend(news_keywords_arr, 3)
-  tfidf_recommendation(news_keywords_arr)
+  # tfidf_recommendation(news_keywords_arr)
+  transformer_recommendation(news_keywords_arr)
--- doc_similarity/model_tfidf.py
+++ doc_similarity/model_tfidf.py
@@ -3,8 +3,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 from doc_similarity.utils import print_recomendation
 
 def tfidf_recommendation(news_arr):
-  ## analyzer -- to select individual words# default
-  ## max_df[0.0,1.0] - used to ignore words with frequency more than 0.8 these words can be useless words as these words may appear only once and may not have a significant meaning
+  ## max_df -- to ignore words with frequency more than 0.8 
+  ## these words can be useless words as these words may appear only once and may not have a significant meaning
   tf = TfidfVectorizer(analyzer='word', stop_words='english', max_df=0.8, ngram_range=(1,3))
   tfidf_matrix = tf.fit_transform(news_arr)
   cosine_similarities = cosine_similarity(tfidf_matrix, tfidf_matrix)
